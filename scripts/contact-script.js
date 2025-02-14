21afc4fe137f850fafd8014c73848d2c729d6da0
Will Bordman <willbordman@pm.me>
2025-02-14 13:45:47 -0500

    scroll button and cleanup

--- scripts/contact-script.js
+++ scripts/contact-script.js
@@ -1,32 +1,4 @@
-//light toggle
-// function lightToggle() {
-//   //background
-//   var background = document.body;
-//   //header
-//   var header = document.getElementsByClassName("page-header-content")[0];
-//   //other
-//   var pageTitle = document.getElementsByClassName("page-title")[0];
-//   var infoBoxes = document.getElementsByClassName("info-text");
-//   var selfPortrait = document.getElementsByClassName("portrait-wrapper")[0];
-//   //footer
-//   var footer = document.getElementsByClassName("page-footer")[0];
-//   //light toggle
-//   var lightToggle = document.getElementsByClassName("light-toggle")[0];
-//   //toggle classes
-//   header.classList.toggle("page-header-content-dm");
-//   background.classList.toggle("body-dm");
-//   pageTitle.classList.toggle("page-title-dm");
-//   for (var i = 0; i < infoBoxes.length; i++) {
-//     infoBoxes[i].classList.toggle("info-text-dm");
-//   }
-//   //image switch
-//   portraitCurrentImage = (portraitCurrentImage === "media/self-portrait.png") ? "media/self-portrait-lm.png" : "media/self-portrait.png";
-//   selfPortrait.classList.toggle("portrait-wrapper-dm");
-//   selfPortrait.children[0].src = portraitCurrentImage;
-//   footer.classList.toggle("page-footer-dm");
-//   lightToggle.classList.toggle("light-toggle-dm");
-// }
-
+// Dark/Light mode toggle
 function lightToggle() {
   const body = document.documentElement;
   var selfPortrait = document.getElementsByClassName("portrait-wrapper")[0];
@@ -44,6 +16,13 @@ function lightToggle() {
   }
 }
 
+// Fades in content
+function showDivById (id) {
+  var div = document.getElementById(id);
+  $(div).fadeIn(200);
+}
+
+// On page load
 document.addEventListener("DOMContentLoaded", () => {
   // Apply saved theme on page load
   if (localStorage.getItem('theme') === 'light') {
@@ -52,10 +31,6 @@ document.addEventListener("DOMContentLoaded", () => {
     selfPortrait.children[0].src = "media/self-portrait-lm.png";
   }
 
-  function showDivById (id) {
-    var div = document.getElementById(id);
-    $(div).fadeIn(200);
-  }
-
+  // Fade in content
   showDivById("contact");
 });
--- scripts/work-script.js
+++ scripts/work-script.js
@@ -1,91 +1,4 @@
-let lightState = 0;
-
-//light toggle
-// function lightToggle() {
-//   if (lightState === 1) lightState = 0;
-//   else lightState++;
-//   //background
-//   var background = document.body;
-//   //header
-//   var header = document.getElementsByClassName("page-header-content")[0];
-//   //other
-//   //menu
-//   var pageTitle = document.getElementsByClassName("page-title")[0];
-//   var switchButtons = document.getElementsByClassName("switch-btn");
-//   var leftButton = document.getElementById("switch-left");
-//   var rightButton = document.getElementById("switch-right");
-//   //code projects
-//   var codeDisplays = document.getElementsByClassName("code-showcase");
-//   var p1 = document.getElementById("interactive-data-viz");
-//   var p2 = document.getElementById("rpi-campus-map");
-//   var p3 = document.getElementById("n-queen-solver");
-//   var p4 = document.getElementById("spotify-bot");
-//   var p5 = document.getElementById("tcp-wordle-server");
-//   var p6 = document.getElementById("my-website");
-//   var codeInfoBox = document.getElementById("code-info-box");
-//   //3d projects
-//   var galleryDisplays = document.getElementsByClassName("gallery-showcase");
-//   var designDisplays = document.getElementsByClassName("design-showcase");
-//   var g1 = document.getElementById("gallery-button-favorites");
-//   var g2 = document.getElementById("gallery-button-2023");
-//   var g3 = document.getElementById("gallery-button-2022");
-//   var g4 = document.getElementById("gallery-button-old");
-//   var g5 = document.getElementById("triumph-modular");
-//   var g6 = document.getElementById("guitar-ad");
-//   var g7 = document.getElementById("darth-croft");
-//   var g8 = document.getElementById("kara");
-//   var g9 = document.getElementById("window");
-//   var g10 = document.getElementById("house");
-//   //footer
-//   var footer = document.getElementsByClassName("page-footer")[0];
-//   //light toggle
-//   var lightToggle = document.getElementsByClassName("light-toggle")[0];
-//   //toggle classes
-//   header.classList.toggle("page-header-content-dm");
-//   background.classList.toggle("body-dm");
-//   pageTitle.classList.toggle("page-title-dm");
-//   for (var i = 0; i < switchButtons.length; i++) {
-//     switchButtons[i].classList.toggle("switch-btn-dm");
-//   }
-//   leftButton.classList.toggle("switch-left-dm");
-//   rightButton.classList.toggle("switch-right-dm");
-//   for (var i = 0; i < codeDisplays.length; i++) {
-//     codeDisplays[i].classList.toggle("code-showcase-dm");
-//   }
-//   p1.classList.toggle("interactive-data-viz-dm");
-//   p2.classList.toggle("rpi-campus-map-dm");
-//   p3.classList.toggle("n-queen-solver-dm");
-//   p4.classList.toggle("spotify-bot-dm");
-//   p5.classList.toggle("tcp-wordle-server-dm");
-//   p6.classList.toggle("my-website-dm");
-//   for (var i = 0; i < galleryDisplays.length; i++) {
-//     galleryDisplays[i].classList.toggle("gallery-showcase-dm");
-//   }
-//   g1.classList.toggle("gallery-button-favorites-dm");
-//   g2.classList.toggle("gallery-button-2023-dm");
-//   g3.classList.toggle("gallery-button-2022-dm");
-//   g4.classList.toggle("gallery-button-old-dm");
-//   for (var i = 0; i < designDisplays.length; i++) {
-//     designDisplays[i].classList.toggle("design-showcase-dm");
-//   }
-//   g5.classList.toggle("triumph-modular-dm");
-//   g6.classList.toggle("guitar-ad-dm");
-//   g7.classList.toggle("darth-croft-dm");
-//   g8.classList.toggle("kara-dm");
-//   g9.classList.toggle("window-dm");
-//   g10.classList.toggle("house-dm");
-//   codeInfoBox.classList.toggle("info-showcase-dm");
-//   footer.classList.toggle("page-footer-dm");
-//   lightToggle.classList.toggle("light-toggle-dm");
-// }
-
-//light toggle auto
-// function lightToggleAuto() {
-//   const now = new Date();
-//   const hours = now.getHours();
-//   if (hours >= 8 && hours < 20) lightToggle();
-// }
-
+// Dark/Light mode toggle
 function lightToggle() {
   const body = document.documentElement;
 
@@ -100,20 +13,42 @@ function lightToggle() {
   }
 }
 
+// Fades in content
+function showDivById(id) {
+  var div = document.getElementById(id);
+  $(div).fadeIn(200);
+}
+
+// Scroll button display
+function scrollFunction(scrollcontainer) {
+  if (document.body.scrollTop > 1500 || document.documentElement.scrollTop > 1500) {
+    scrollcontainer.style.display = "block";
+  } else {
+    scrollcontainer.style.display = "none";
+  }
+}
+
+// Scroll to top
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+} 
+
+// On page load
 document.addEventListener("DOMContentLoaded", () => {
   // Apply saved theme on page load
   if (localStorage.getItem('theme') === 'light') {
     document.documentElement.classList.add('light-mode');
   }
 
-  //switch functions
-  function showDivById(id) {
-    var div = document.getElementById(id);
-    $(div).fadeIn(200);
-  }
-
+  // Fade in content
   showDivById("code-projects");
 
+  // Display scroll button
+  let scroll_button = document.getElementById("scroll-container");
+  window.onscroll = function() {scrollFunction(scroll_button)};
+
+  // Project switch handler
   const leftButton = document.getElementById("switch-left");
   const rightButton = document.getElementById("switch-right");
   l_selected = true;
@@ -154,18 +89,18 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 
-  //collapsible functions
+  // Collapsible functions
   var coll = document.getElementsByClassName("collapsible");
   var col2 = document.getElementsByClassName("gallery-collapsible");
   var start = false;
   var i;
 
-  //gallery collapsibles
+  // Gallery collapsibles
   for (i = 0; i < col2.length; i++) {
     col2[i].addEventListener("click", function () {
       this.classList.toggle("active");
       var content = this.nextElementSibling;
-      //gallery-favorites starts open
+      // Gallery-favorites starts open
       if (content.id === "gallery-favorites" && !start) {
         content.style.display = "block";
         start = true;
@@ -190,7 +125,7 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 
-  //project collapsibles
+  // Project collapsibles
   for (i = 0; i < coll.length; i++) {
     coll[i].addEventListener("click", function () {
       this.classList.toggle("active");
@@ -210,27 +145,21 @@ document.addEventListener("DOMContentLoaded", () => {
     });
     coll[i].addEventListener("mouseout", function () {
       var content = this.nextElementSibling;
-      if (lightState === 0) {
-        this.style.borderColor = "var(--dm-green)";
-        content.style.borderImage = "linear-gradient(to bottom, var(--dm-green), rgba(0, 23, 31, 0) 50%) 1";
-      }
-      else if (lightState === 1) {
-        this.style.borderColor = "var(--lm-green)";
-        content.style.borderImage = "linear-gradient(to bottom, var(--lm-green), rgba(0, 23, 31, 0) 50%) 1";
-      }
+      this.style.borderColor = "var(--dm-green)";
+      content.style.borderImage = "linear-gradient(to bottom, var(--dm-green), rgba(0, 23, 31, 0) 50%) 1";
     });
   }
 
-  //gallery image handler
+  // Gallery image handler
   document.querySelectorAll(".gallery-img-wrapper").forEach(item => {
-    //access image div
+    // Access image div
     var image = item.children[0];
-    //access image info icon
+    // Access image info icon
     var infoIcon = item.children[1];
-    //access image details div
+    // Access image details div
     var imageDetails = item.children[2];
 
-    //show info icon
+    // Show info icon
     infoIcon.style.display = "none";
     item.addEventListener('mouseover', event => {
       infoIcon.style.display = "block";
@@ -240,7 +169,7 @@ document.addEventListener("DOMContentLoaded", () => {
       infoIcon.style.display = "none";
     })
 
-    //image states
+    // Image states
     var selected = {
       "filter": "opacity(0)",
       "-webkit-filter": "opacity(0)",
@@ -251,7 +180,7 @@ document.addEventListener("DOMContentLoaded", () => {
       "-webkit-filter": "opacity(1)",
     };
 
-    //show image details
+    // Show image details
     Object.assign(image.style, deselected);
     imageDetails.style.display = "none";
     item.addEventListener('click', event => {
